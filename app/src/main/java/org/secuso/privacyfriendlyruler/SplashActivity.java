1ab5a5026185dd0577e380325ed55530a1725e38
Karola Marky <yonjuni@pinyto.de>
2016-12-12 01:26:47 +0200

    Splash Activity added

--- app/src/main/java/org/secuso/privacyfriendlyruler/SplashActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyruler/SplashActivity.java
@@ -1,8 +1,24 @@
 package org.secuso.privacyfriendlyruler;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
- * Created by karo on 12.12.16.
+ * Created by yonjuni on 17.11.16.
  */
 
-public class SplashActivity {
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        SplashActivity.this.startActivity(mainIntent);
+        SplashActivity.this.finish();
+
+    }
+
 }
